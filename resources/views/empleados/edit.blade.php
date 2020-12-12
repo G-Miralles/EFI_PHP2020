46b207486ff0848a7feead04119ecd687f80e3ca
G-Miralles <g.miralles@itecriocuarto.org.ar>
2020-12-11 21:29:24 -0300

    Se hicieron mejoras en el formularios para reducir las lineas de codigo

--- resources/views/empleados/edit.blade.php
+++ resources/views/empleados/edit.blade.php
@@ -1,22 +1,7 @@
 <form action="{{ url('/empleados/' .$Empleado->id) }}" method="post" enctype="multipart/form-data">
 {{ csrf_field() }}
 {{ method_field('PATCH') }}
-    <label for="Nombre"> {{'Nombre'}} </label>
-    <input type="text" name="name" id="name" value="{{$Empleado->name}}">
-</br>
-    <label for="Apellido"> {{'Apellido'}} </label>
-    <input type="text" name="surname" id="surname" value="{{$Empleado->surname}}">
-</br>
-    <label for="Email"> {{'Email'}} </label>
-    <input type="email" name="email" id="email" value="{{$Empleado->email}}">
-</br>
-    <label for="Foto"> {{'Foto'}} </label>
-    </br>
-        <img src="{{ asset('storage').'/'.$Empleado->photo }}" alt="" width="200">
-    </br>
-    <input type="file" name="photo" id="photo" value="">
-</br>
-    <input type="submit"  value="Editar">
-    <a href="{{ url('empleados') }}"> Vovler al Inicio </a>
+
+@include('empleados.form', ['Mod'=>'edit'])
 
 </form>
--- resources/views/empleados/form.blade.php
+++ resources/views/empleados/form.blade.php
@@ -1,17 +1,22 @@
-{{ $Mod=='create' ? 'Nuevo empleado' : 'Editar empleado' }}
+
 
     <label for="Nombre"> {{'Nombre'}} </label>
-    <input type="text" name="name" id="name" value="">
+    <input type="text" name="name" id="name" value="{{ isset($Empleado->name) ? $Empleado->name:''}}">
 </br>
     <label for="Apellido"> {{'Apellido'}} </label>
-    <input type="text" name="surname" id="surname" value="">
+    <input type="text" name="surname" id="surname" value="{{ isset($Empleado->surname) ? $Empleado->surname:''}}">
 </br>
     <label for="Email"> {{'Email'}} </label>
-    <input type="email" name="email" id="email" value="">
+    <input type="email" name="email" id="email" value="{{ isset($Empleado->email) ? $Empleado->email:''}}">
 </br>
     <label for="Foto"> {{'Foto'}} </label>
-    <input type="file" name="photo" id="photo" value="">
+    @if (isset($Empleado->photo))
+        </br>
+            <img src="{{ asset('storage').'/'.$Empleado->photo }}" alt="" width="200">
+        </br>
+    @endif
+    
 </br>
-    <input type="submit"  value="Agregar">
+    <input type="submit"  value="{{ $Mod=='create' ? 'Agregar' : 'Editar' }}">
 
 <a href="{{ url('empleados') }}"> Vovler al Inicio </a>
